ac67be551b8469be95d79553dc48f41e306a5a51
sdelpercio <sdelperc@gmail.com>
2019-12-19 15:48:51 -0500

    finished 46 of 56 tasks

--- index.js
+++ index.js
@@ -165,8 +165,23 @@ class Instructor extends Lambdasian {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian {
+  constructor(atts) {
+    super(atts),
+    this.previousBackground = atts.previousBackground,
+    this.className = atts.className,
+    this.favSubjects = atts.favSubjects
+  }
+  listSubjects() {
+    const list = this.favSubjects.toString(' ');
+    return `Loving ${list}!`;
+  }
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
 }
 
 /*
